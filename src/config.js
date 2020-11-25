58464f8aa3a323dea3ae842fde0612570febc58a
CerealSurya <sjalapati42@gmail.com>
2020-11-24 23:31:16 -0600

    nothing relly

--- src/config.js
+++
@@ -1,9 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url); //Getting require working with ESM modules
-
-require('dotenv').config()
-
-export default {
-    prefix: "?",
-    token: process.env.TOKEN //Exporting our tokens and stuff
-}
